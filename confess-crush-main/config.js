904fe68c126a315912c7d027e9e1967cae8d1a2b
Tej Alpeshkumar Raval <59118735+tejrawal@users.noreply.github.com>
2024-05-08 13:51:42 +0530

    Add files via upload

---
+++ confess-crush-main/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "web header",
+    introTitle: 'Sayaniii',
+    introDesc: `Darling, you are the light that brightens my world,
+                the warmth that comforts me, and the sparkle in my eyes. 
+                With you, I've found a love so deep and precious that words can't fully express it. 
+                Every moment with you feels like a sweet melody that I never want to end. 
+                I promise to love you endlessly, with all my heart and soul. 💖`,
+    btnIntro: '6yanii 😘❤️💖',
+    title: 'Do You Love Me As Much As I Love You ? ',
+    desc: 'I Know You Do Love Me But My Love Is Way More Then Yours 😙 So Will You Love Me as Much As I Love You ? 🥺',
+    btnYes: 'Yes I Will < 3 ❤️',
+    btnNo: 'Abhi Kuch Keh Nai Sakte 🤔',
+    question: 'Tumne Mujhe Dekha First Time Tabse Mujhse Pyaar Thaa Na Tumhe ❤️🤔 , Nahi ? Toh Reason Do ',
+    btnReply: 'Send Your Love 💕<3',
+    reply: 'Haaan Tej I Fall In Love With You At First Site ❤️🤗',
+    mess: 'Mujhe Pata Hi Tha 🤭. Love Youuuuuu 6ayaniii 😘😘',
+    btnAccept: 'Love You Toooo Tejjj ❤️😘',
+    messLink: 'https://www.snapchat.com/add/tejraval11?share_id=K1TWzhOugZY&locale=en-IN' 
+}
